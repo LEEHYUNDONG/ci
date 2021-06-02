68c3a95c4a7e57906c2b15c4752c2e1093cf9007
LEEHYUNDONG <32635799+LEEHYUNDONG@users.noreply.github.com>
2021-06-02 10:24:26 +0900

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
-// daydreaming
+
+
 int main(){
 
     printf("<h1>Hello World!!!!</h1>\n");
     printf("<h2>Hello HONG hONG!!!!</h2>\n");
-    printf("<h3>Hello HTML</h3>\n");
 
     return 0;
 }
